c88d7df29b92107255ba0b9237bc28fd65ab09bb
katrina <katrina.nov13@gmail.com>
2020-08-26 16:30:05 +0300

    refact banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,15 +1,10 @@
-import React from 'react';
 import styled from 'styled-components';
 import banner from '../image/banner.jpg';
 
 
-const BannerStyle = styled.div`
+export const Banner = styled.div`
   background: url(${banner}) no-repeat center/cover;
   height: 210px;
   width: 100%;
 `;
 
-export const Banner = () => (
-  <BannerStyle>    
-  </BannerStyle>
-);
